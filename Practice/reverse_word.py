0fb7ada701f071498e56b48dd5036a1d17828309
Ponhrith Nhep <53818555+Ponhrith@users.noreply.github.com>
2024-12-02 21:03:01 +0700

    Testing

--- Practice/reverse_word.py
+++ Practice/reverse_word.py
@@ -3,8 +3,11 @@ word = input("Enter a word: ")
 reverse_word = ""
 size = len(word) - 1
 
-while size >= 0:
-    char = word[size]
-    reverse_word += char
-    size -= 1
-print("The reverse word is: ", reverse_word)
+# while size >= 0:
+#     char = word[size]
+#     reverse_word += char
+#     size -= 1
+
+for letter in word:
+    reverse_word = letter + reverse_word
+print("The reversed word is: ", reverse_word)
