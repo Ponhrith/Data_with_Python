7e11b1f5f012c0dbee4cc38a87b064a6a1219ea2
Ponhrith Nhep <53818555+Ponhrith@users.noreply.github.com>
2024-12-11 21:01:36 +0700

    Testing

--- Practice/count_and_find.py
+++ Practice/count_and_find.py
@@ -1,14 +1,14 @@
 numbers = [1, 2, 1, 3, 1, 4, 5, 1]
 target = 1
 count = 0
-indices = []
+indext_list = []
 index = 0  
 
 for i in numbers:
     if i == target:
         count += 1
-        indices.append(index) 
+        indext_list.append(index) 
     index += 1  
 
 print(count)
-print(indices)
+print(indext_list)
